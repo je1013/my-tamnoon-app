fbf62efa52eecfb172677f32168b5f2ec76b05ea
Jeremy Eisenman <jeisenman5000@gmail.com>
2024-10-22 13:56:57 -0400

    allow for changing of isCrownJewel value
    some efficiency fixes to this problem:
    -in order to save on cost during edit mode, we only save to localStorage
    when we close out of editMode, thus we need to maintain a `currentEdits`
    so we know what to save
    -furthermore, if currentEdits is toggled back to its original value, we
    want to save the setting of the full localStorage value `savedEdits` the
    trouble of setting the value to what it already is. for a large amount of
    inputs on a large amount of rows, this saves cost

--- src/Asset.jsx
+++ src/Asset.jsx
@@ -1,12 +1,33 @@
+import { useContext, useState } from "react";
+import { EditModeContext } from "./Assets";
+
 export default function Asset({data}) {
   const {id, name, owner, isCrownJewel} = data;
+  const {isEditMode, addChangeToQueue} = useContext(EditModeContext);
+  const [crownJewelState, changeCrownJewelState] = useState(isCrownJewel);
+
+  function modifyCrownJewelState(value) {
+    const isTrue = (value === 'true');
+    changeCrownJewelState(isTrue);
+    addChangeToQueue(id, isTrue, isCrownJewel);
+  }
 
   return (
     <tr>
       <th scope="row">{id}</th>
       <td>{name}</td>
       <td>{owner}</td>
-      <td>{isCrownJewel.toString()}</td>
+      <td>
+        {
+          isEditMode ?
+          <select value={crownJewelState} onChange={(e)=>{modifyCrownJewelState(e.target.value)}}>
+            <option value={true}>True</option>
+            <option value={false}>False</option>
+          </select>
+          :
+          isCrownJewel.toString()
+        }
+      </td>
     </tr>
   )
 }
--- src/Assets.jsx
+++ src/Assets.jsx
@@ -1,36 +1,73 @@
 import raw_assets from './json/assets.json';
 import Asset from './Asset.jsx'
+import { createContext, useState } from 'react';
 
+export const EditModeContext = createContext(null);
 export default function Assets() {
+  const columns = ['Id', 'Asset Name', 'Owner Name', 'Is Crown Jewel'];
+  const [savedEdits, setSavedEdits] = useState(JSON.parse(localStorage.getItem("savedEdits") || '{}'));
+  const [isEditMode, changeIsEditMode] = useState(false);
+  const [currentEdits, changeCurrentEdits] = useState({});
   const assets = raw_assets.map(asset => {
+    const hasCrownJewelChanges = savedEdits[asset._id] !== undefined;
     return {
       id: asset._id || 'N/A',
       name: asset.assetName || 'N/A',
       owner: asset.owner.name || asset.owner.owner.name || 'N/A',
-      isCrownJewel: asset.enriched.isCrownJewel,
+      isCrownJewel: hasCrownJewelChanges ? savedEdits[asset._id] : asset.enriched.isCrownJewel,
     }
   });
 
-  console.log(assets);
-  const columns = ['Id', 'Asset Name', 'Owner Name', 'Is Crown Jewel'];
+  function addChangeToQueue(id, value, original) {
+    changeCurrentEdits(curr => {
+      if (value === original) {
+        delete curr[id];
+      } else {
+        curr[id] = value;
+      }
+      return curr;
+    });
+  }
+
+  function toggleIsEditMode() {
+    if (isEditMode) {
+      setSavedEdits(saved => {
+        for (const [id, value] of Object.entries(currentEdits)) {
+          saved[id] = value;
+        }
+
+        return saved;
+      })
+
+      localStorage.setItem('savedEdits', JSON.stringify(savedEdits));
+    };
+
+    changeCurrentEdits({});
+    changeIsEditMode(v => !v);
+  }
+
   return (
-    <table>
-      <thead>
-        <tr>
+    <EditModeContext.Provider value={{isEditMode, addChangeToQueue}}>
+      <h3>Current Mode: {isEditMode ? 'Edit' : 'View'} Mode</h3>
+      <button value={isEditMode} onClick={toggleIsEditMode}>Change Mode</button>
+      <table>
+        <thead>
+          <tr>
+            {
+              columns.map((column, index) => {
+                return (<th key={index} scope="col">{column}</th>)
+              })
+            }
+          </tr>
+        </thead>
+        <tbody>
           {
-            columns.map((column, index) => {
-              return (<th key={index} scope="col">{column}</th>)
+            assets.map(asset => {
+              return (<Asset key={asset.id} data={asset}/>)
             })
           }
-        </tr>
-      </thead>
-      <tbody>
-        {
-          assets.map(asset => {
-            return (<Asset key={asset.id} data={asset}/>)
-          })
-        }
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </EditModeContext.Provider>
   )
 }
